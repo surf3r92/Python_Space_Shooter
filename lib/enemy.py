4fe7a05725ceebb64858fe14a56f847e0d1266da
Fabian Schatz <Fabian.Schatz@googlemail.com>
2016-01-11 14:28:09 +0100

    eLaser

--- lib/enemy.py
+++ lib/enemy.py
@@ -16,22 +16,7 @@ class Enemy(pygame.sprite.Sprite):
 		self.counter = 50
 		self.xList = np.linspace(np.pi,np.pi*3,100)
 		self.yList = np.sin(self.xList)
-		print self.xList
-		print self.yList
-	# def update(self):
-		# self.counter +=1
-		# dx = random.randint(1,17)-5
-		# dy = random.randint(1,10)-5
-		# if self.rect.right > 800:
-			# self.reverse = 1
-		# if self.rect.left < 0:
-			# self.reverse = 0
-		# if self.reverse == 1:
-			# dx *= -1
-		# self.rect.move_ip(dx, dy)
-		
-		# if self.counter%90 == 0:
-			# enemyLaserSprites.add(EnemyLaser(self.rect.center))
+
 			
 	def update(self):
 		self.counter += 1
@@ -47,6 +32,9 @@ class Enemy(pygame.sprite.Sprite):
 				dx *= -1
 			self.rect.move_ip(dx,dy*6)
 			
+			if self.counter%75 == 0:
+			enemyLaserSprites.add(EnemyLaser(self.rect.center))
+			
 		else:
 			self.xStart = np.linspace(0,np.pi,50)
 			self.yStart = np.sin(self.xStart)
@@ -55,18 +43,7 @@ class Enemy(pygame.sprite.Sprite):
 			if self.reverse == 1:
 				dx *= -1
 			self.rect.move_ip(dx,dy*8)
-			print "xstart"
-			print self.xStart
-			print "yStart"
-			print self.yStart
-			print "xList"
-			print self.xList
-			print "yList"
-			print self.yList
-	
-	# def cosineInterpolation(y1, y2, mu):
-		# mu2 = (1-cos(mu*PI))/2
-		# return (y1*(1-mu2)+y2*mu2);
+
 		
 class EnemyLaser(pygame.sprite.Sprite):
 	def __init__(self, pos):
