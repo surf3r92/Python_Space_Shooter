b2fca0507b78d3344f78d5ad1d2e1745e9c89712
Fabian Schatz <Fabian.Schatz@googlemail.com>
2016-01-15 11:54:06 +0100

    fixed highscore index out of range bug
    try/except

--- lib/highscore.py
+++ lib/highscore.py
@@ -58,8 +58,10 @@ def highScore(self):
     for highscoreElement in highscoreList:
         highscoreElementListWithoutEnd = highscoreElement.strip()
         highscoreElementList = highscoreElementListWithoutEnd.split(",")
-        list.append((highscoreElementList[0],highscoreElementList[1]))
-
+        try:
+            list.append((highscoreElementList[0],highscoreElementList[1]))
+        except:
+            print "Highscore Element has no Score or Name Value"
     heighScoreHeight = 350
     heighScoreLength = 330
     heighScoreXPos = (self.screen.get_size()[0] / 2) - (heighScoreLength / 2)
