b0ca5dc400c5cca54504f2f279b915994d8a3f4a
WaschTy <baeuml.sebastian95@yahoo.de>
2016-01-20 17:29:26 +0100

    Highscore Tabelle

--- lib/highscore.py
+++ lib/highscore.py
@@ -5,12 +5,12 @@ from lib.menu import *
 
 
 class HighScore:
-    def __init__(self, screen, list, x, y, color, length, height, text_font, font_size, text_color):
+    def __init__(self, screen, list, x, y, colors, length, height, text_font, font_size, text_color):
         self.list = list
         self.screen = screen
         self.x = x
         self.y = y
-        self.color = color
+        self.colors = colors
         self.length = length
         self.height = height
         self.text_font = text_font
@@ -19,9 +19,28 @@ class HighScore:
 
     def create_list(self):
         self.draw()
+        self.createHeader()
         for index in range(0, len(self.list)):
             self.write_text(self.list[index][0], self.list[index][1], index)
         self.rect = pygame.Rect(self.x, self.y, self.length, self.height)
+
+    def createHeader(self):
+        rectHeight = 25
+        pygame.draw.rect(self.screen, self.colors[3], (self.x, self.y, self.length, rectHeight*1.3), 0)
+
+
+        #pygame.draw.rect(self.screen, self.colors[2], (self.x, self.y, self.length, rectHeight*1.3), 1)
+        myFont = pygame.font.SysFont("comicsansms", int(self.font_size*1.2), bold = True)
+
+        name = myFont.render("Name", 1, self.text_color)
+        headerTextYPos = self.y+(rectHeight/2)-(name.get_height()/2)
+        nameXPos = self.x+(self.length/4)*3-(name.get_width()/2)
+
+        score = myFont.render("Score", 1, self.text_color)
+        scoreXPos = self.x+(self.length/4)-(name.get_width()/2)
+
+        self.screen.blit(score, (nameXPos, headerTextYPos))
+        self.screen.blit(name, (scoreXPos, headerTextYPos))
 
     def write_text(self, key, value, index):
         myFont = pygame.font.SysFont(self.text_font, self.font_size)
@@ -32,13 +51,24 @@ class HighScore:
         listingNum = myFont.render(str(index + 1) + ".", 1, self.text_color)
 
         mediumDist = 20
-        yPos = (self.y + index * 30 + 20)
+        yPos = (self.y + index * 30 + 35)
+        yPosBackground = (self.y + index * 30 + 30)
 
-        self.screen.blit(key, ((self.x + self.length / 2) - key.get_width() - mediumDist, yPos))
-        self.screen.blit(colon, ((self.x + self.length / 2) - colon.get_width() / 2, yPos))
-        self.screen.blit(value, ((self.x + self.length / 2) + mediumDist, yPos))
-        self.screen.blit(listingNum, (self.x - listingNum.get_width() + 70, yPos))
+        self.setBackground(key, index, yPosBackground)
+
+        self.screen.blit(key, (self.x+(self.length/4)-(key.get_width()/2), yPos))
+        #self.screen.blit(colon, ((self.x + self.length / 2) - colon.get_width() / 2, yPos))
+        self.screen.blit(value, (self.x+(self.length/4)*3-(value.get_width()/2), yPos))
+        #self.screen.blit(listingNum, (self.x - listingNum.get_width() - 40, yPos))
         return self.screen
+
+    def setBackground(self, key, index, yPos):
+        if (index%2 == 0):
+            currLineColor = self.colors[0]
+        else:
+            currLineColor = self.colors[1]
+
+        pygame.draw.rect(self.screen, currLineColor, (self.x, yPos, self.length, key.get_height()*1.3), 0)
 
     def draw(self):
         # pygame.draw.rect(self.screen, self.color, (self.x,self.y,self.length,self.height), 0)
@@ -62,16 +92,16 @@ def highScore(self):
             list.append((highscoreElementList[0],highscoreElementList[1]))
         except:
             print "Highscore Element has no Score or Name Value"
-    heighScoreHeight = 350
-    heighScoreLength = 330
+    heighScoreHeight = 300
+    heighScoreLength = 600
     heighScoreXPos = (self.screen.get_size()[0] / 2) - (heighScoreLength / 2)
-    heighScoreYPos = (self.screen.get_size()[1] / 2) - (heighScoreHeight / 2) + 25
-    heighScoreColor = (46, 46, 254)
+    heighScoreYPos = (self.screen.get_size()[1] / 2) - (heighScoreHeight / 2) + 5
+    heighScoreColors = [(25,16,134), (17,10,87), (190, 190, 190), (5,5,40)]
     heighScoreTextFont = "Calibri"
-    heighScoreTextFontSize = 20
+    heighScoreTextFontSize = 23
     heighScoreTextColor = (255, 255, 255)
 
-    highScoreList = HighScore(self.screen, list, heighScoreXPos, heighScoreYPos, heighScoreColor, heighScoreLength,
+    highScoreList = HighScore(self.screen, list, heighScoreXPos, heighScoreYPos, heighScoreColors, heighScoreLength,
                               heighScoreHeight, heighScoreTextFont, heighScoreTextFontSize, heighScoreTextColor)
 
     buttonWidth = 0
